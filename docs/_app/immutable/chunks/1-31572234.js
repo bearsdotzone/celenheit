6bb2c0b6cd602055d8dca641d88de7f26b3a4812
bearsdotzone <bears@bears.zone>
2025-08-02 21:16:59 -0500

    Update page

--- docs/_app/immutable/chunks/1-31572234.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-833ef4dd.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-7c4f8d54.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-2f3f3d1e.js";export{t as component};
